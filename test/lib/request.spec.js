0759833289cf7462eaec0323c4ff40c1ae2c8f23
spomni <mikhail.snitko@gmail.com>
2021-11-22 15:38:29 +0300

    test(draft): describe test cases of the request module

---
+++ test/lib/request.spec.js
@@ -0,0 +1,22 @@
+describe('request', () => {
+  
+  describe('Request', () => {
+
+    describe('instance', () => {
+      
+      it.todo('Should be instance of Request')
+      it.todo('Should have a read only property "mountPoint"')
+      it.todo('the "mountPoint" property should trail the parent request one')
+      it('Should works correct if parent request has no property "mountPoint"')
+      it.todo('Should not be the same instance passed to the constructor')
+      it.todo('Should have passed request in the prototype chain')
+      it.todo('Should have access to own properties')
+      it.todo('Should have access to own prototype properties')
+      it.todo('Should have access to properties of the request passed to the constructor')
+    })
+  })
+  
+  describe('createRequest', () => {
+    it.todo('Should call a Request constructor with passed values')
+  })
+})
