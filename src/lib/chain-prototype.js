66a45bec01922cb5493a479347c6703e8261267d
spomni <mikhail.snitko@gmail.com>
2021-11-22 14:11:25 +0300

    draft: draft chain-prototype

---
+++ src/lib/chain-prototype.js
@@ -0,0 +1,43 @@
+function getActualSiblings(child, shifting) {
+  
+  let actualShifting = 0
+  let actualChild = child
+  let actualParent = Object.getPrototypeOf(child)
+
+  if (actualParent) {
+    let nextParent;
+  
+    while (actualShifting < shifting) {
+    
+      nextParent = Object.getPrototypeOf(actualParent)
+  
+      if (nextParent) {
+        actualChild = actualParent
+        actualParent = nextParent
+        actualShifting += 1
+      } else {
+        actualShifting = shifting
+      }
+    }
+  }
+  
+  return { actualChild, actualParent}
+}
+
+function chainPrototype(child, newParent, shifting = 0) {
+
+  const {
+    actualChild,
+    actualParent
+  } = getActualSiblings(child, shifting)
+  
+  Object.setPrototypeOf(actualChild, newParent)
+  
+  if (actualParent) {
+    Object.setPrototypeOf(newParent, actualParent)
+  }
+}
+
+module.exports = {
+  chainPrototype,
+}
