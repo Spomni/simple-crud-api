c737acc353025957484794ef74dfa2fc6892777f
spomni <mikhail.snitko@gmail.com>
2021-11-22 14:26:21 +0300

    test(draft): describe chain-prototype test cases

---
+++ test/lib/chain-prototype.spec.js
@@ -0,0 +1,12 @@
+describe('chain-prototype', () => {
+
+  describe('chainPrototype()', () => {
+    
+    it('should return object passed as child option')
+    it('should add parent as child prototype')
+    it('should not lose original prototype')
+    it('should add parent as prototype of original proto with number shifting plus one')
+    it('should not lose original prototype chain')
+    it('should add parent the last prototype in chain if shifting options is greater than cain length')
+  })
+})
