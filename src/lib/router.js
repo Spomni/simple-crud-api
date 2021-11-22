3890cbfdf76752071d39740243624687e14e82b4
spomni <mikhail.snitko@gmail.com>
2021-11-22 12:46:57 +0300

    draft: draft router

---
+++ src/lib/router.js
@@ -0,0 +1,36 @@
+function createRouterFunction() {
+  function router(...args) { this._route(...args) }
+  chainPrototype(router, Router.prototype)
+}
+
+class Router {
+
+  constructor() {
+    this._routes = {}
+    return createRouterFunction().bind(this)
+  }
+
+  this.register(routes) {
+    routes.forEach((currRoute) => {
+      this._routes[currRoute.path] = { ...currRoute }
+    })
+  }
+
+  this._route(req, rest) {
+    const { path } = req
+    const registeredPathes = Object.keys(this._routes)
+
+    const mountPoint = registeredPathes.find((currPath) => {
+      const re = new RegExp(`^${path}($|?|/)`)
+      return re.test(currPath)
+    })
+
+    if (mountPoint) {
+      const { use } = this._routes[mountPoint]
+      const useRequest = createRequest(req, { mountPoint })
+      use(useRequest, res)
+    } else {
+      view.pageNotFound(req, res)
+    }
+  }
+}
