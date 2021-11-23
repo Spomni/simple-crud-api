0fd9415d9ab8e424b4db9d67dbe2b2875a9a71f6
spomni <mikhail.snitko@gmail.com>
2021-11-23 23:57:18 +0300

    test: test router module

--- test/lib/router.spec.js
+++ test/lib/router.spec.js
@@ -1,28 +1,191 @@
-describe('router', () {
+const { createRouter, Router } = require('../../src/lib/router')
+const { Request } = require('../../src/lib/request')
+
+const {
+  UnresolvedRequestError
+} = require('../../src/contract/unresolved-request-error')
+
+describe('router', () => {
 
   describe('createRouter()', () => {
-    it.todo('should return new instance of router')
+    it('should return new instance of Router', () => {
+      const first = createRouter()
+      const second = createRouter()
+      
+      expect(first).toBeInstanceOf(Router)
+      expect(second).toBeInstanceOf(Router)
+      expect(second).not.toBe(first)
+    })
   })
   
   describe('Router', () => {
     
     describe('constructor()', () => {
       
-      it.todo('should return function')
-      it.todo('should return instance of Router')
+      it('should return function', () => {
+        const router = createRouter()
+        expect(router).toBeInstanceOf(Function)
+      })
+      
+      it('should return instance of Router', () => {
+        const router = createRouter()
+        expect(router).toBeInstanceOf(Router)
+      })
     })
     
     describe('#register()', () => {
-      it.todo('should throw an error if an option "path" is not mount point')
-      it.todo('should throw an error if an option "use" is not a function')
-      it.todo('should throw an error if any leading part of path has been registered earlier')
-      it.todo('should works with array of configs')
+    
+      it('should throw an error if an option "path" is not mount point', () => {
+        const router = createRouter()
+        const register = () => router.register({ path: 'not', use: () => {}})
+
+        expect(register).toThrow('is not a valid mount point')
+      })
+
+      it('should throw an error if an option "use" is not a function', () => {
+        const router = createRouter()
+        const register = () => router.register({ path: '/', use: {} })
+        
+        expect(register).toThrow('is not a function')
+      })
+
+      it('should throw an error if any leading part of path has been registered earlier', () => {
+        
+        const router = createRouter()
+        
+        router.register({
+          path: '/some',
+          use: () => {},
+        })
+        
+        const register = () => router.register({
+          path: '/some/path',
+          use: () => {},
+        })
+        
+        expect(register).toThrow('has already been registered')
+      })
+      
+      it('should works with many config arguments', () => {
+
+        const use = () => {}
+        const pathes = ['/one', '/two', '/three']
+        const fixture = pathes.map((path) => ({ path, use }))
+        const router = createRouter()
+        
+        router.register(...fixture)
+        
+        expect(router.pathes).toEqual(pathes)
+      })
+    })
+
+    describe('#patches', () => {
+      it('should return all registered pathes', () => {
+        const use = () => {}
+        const pathes = ['/one', '/two', '/three']
+        const fixture = pathes.map((path) => ({ path, use }))
+        const router = createRouter()
+        
+        fixture.forEach((config) => router.register(config))
+
+        expect(router.pathes).toEqual(pathes)
+      })
     })
     
     describe('instance()', () => {
       
-      it.todo('should throw an UnresolvedRequestError instance if it has no requestered paths with request url')
-      it.todo('should call registered callback for request with related path')
+      it('should throw an UnresolvedRequestError instance if it has no registered path matches request url', () => {
+        const router = createRouter()
+        const response = {}
+
+        router.register({
+          path: '/some/path',
+          use: () => {}
+        })
+        
+        const incomingMessage = {
+          url: '/another'
+        }
+        
+        const request = {
+          url: '/some/another/path',
+          mountPoint: '/some',
+          path: '/another/path'
+        }
+        
+        const routeIncomingMessage = () => router(incomingMessage, response)
+        const routeRequest = () => router(request, response)
+        
+        expect(routeIncomingMessage).toThrow(UnresolvedRequestError)
+        expect(routeRequest).toThrow(UnresolvedRequestError)
+      })
+
+      it('should call registered callback related with passed request', () => {
+        const router = createRouter()
+        const response = {}
+
+        const incomingMessage = { url: '/some/path/to/resourse' }
+
+        const request = {
+          url: '/long/some/path/to/resourse',
+          mountPoint: '/long',
+          path: '/some/path/to/resourse'
+        }
+
+        const useSpy = jest.fn()
+        
+        router.register(
+          {
+            path: '/some/path/to',
+            use: useSpy
+          }
+        )
+        
+        router(incomingMessage, response)
+        router(request, response)
+        
+        expect(useSpy.mock.calls.length).toBe(2)
+      })
+      
+      it('registered callback should be called with nested request and original response', () => {
+        const router = createRouter()
+        const response = {}
+        
+        const incomingMessage = { url: '/some/path/to/resourse' }
+        
+        const request = {
+          url: '/long/some/path/to/resourse',
+          mountPoint: '/long',
+          path: '/some/path/to/resourse'
+        }
+        
+        const useSpy = jest.fn()
+        
+        const route = {
+          path: '/some/path/to',
+          use: useSpy
+        }
+        
+        router.register(route)
+        
+        router(incomingMessage, response)
+
+        let useReq = useSpy.mock.calls[0][0]
+        let useRes = useSpy.mock.calls[0][1]
+
+        expect(useReq).toBeInstanceOf(Request)
+        expect(useReq.mountPoint).toBe(route.path)
+        expect(useRes).toBe(response)
+
+        router(request, response)
+        
+        useReq = useSpy.mock.calls[1][0]
+        useRes = useSpy.mock.calls[1][1]
+        
+        expect(useReq).toBeInstanceOf(Request)
+        expect(useReq.mountPoint).toBe(request.mountPoint + route.path)
+        expect(useRes).toBe(response)
+      })
     })
   })
 })
