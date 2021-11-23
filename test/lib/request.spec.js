c2d8d6985bd46d4fc37bdc5a79a04097a59267b2
spomni <mikhail.snitko@gmail.com>
2021-11-23 10:37:45 +0300

    test: test request module

--- test/lib/request.spec.js
+++ test/lib/request.spec.js
@@ -1,34 +1,133 @@
+const { Request, createRequest } = require('../../src/lib/request')
+
+class ParentRequest {
+  constructor() {
+    this.url = '/some/path/to/resourse?prop=value'
+  }
+}
+
 describe('request', () => {
+
+  describe('createRequest', () => {
+  
+    it('Should return new Request instance', () => {
+      const parent = new ParentRequest()
+      const options = { mountPoint: '/' }
+  
+      const firstRequest = createRequest(parent, options)
+      const secondRequest = createRequest(parent, options)
+      
+      expect(secondRequest).not.toBe(firstRequest)
+    })
+  })
   
   describe('Request', () => {
   
-    constructor(parent, { mountPoint }) {
-      it.todo('Should throw an error if the parent mountPoint concatenated with passed one is not leading part of the parent property "url"')
-    }
+    describe('constructor()', () => {
+    
+      it('Should throw an error if the parent mountPoint concatenated with passed one is not leading part of the parent property "url"', () => {
+        const fixture = [
+          {},
+          '',
+          'path1/to/2',
+          '/asd/',
+          '/asd?some',
+          '/path1//to/2',
+        ]
+        
+        fixture.forEach((mountPoint) => {
+          expect(() => createRequest({}, { mountPoint })).toThrow('not valid mountPoint')
+        })
+      })
+      
+      it('Should throw an error if mountPoint leads to the same url like parent mountPoint', () => {
+        const parent = new ParentRequest()
+        parent.mountPoint = '/some/path'
+        
+        expect(() => createRequest(parent, { mountPoint: '/' })).toThrow('must not be equal to parent')
+      })
+      
+      it('Should throw an error if new mountPoint is not leading part of parent url', () => {
+        const parent = new ParentRequest()
+        const mountPoint = '/another/path'
+        const construct = () => createRequest(parent, { mountPoint })
+
+        expect(construct).toThrow('is not part of parent url')
+      })
+    })
 
     describe('instance', () => {
       
-      it.todo('Should be instance of Request')
-      it.todo('Should not be the same instance passed to the constructor')
-      it.todo('Should have passed request in the prototype chain')
-      it.todo('Should have access to own properties')
-      it.todo('Should have access to own prototype properties')
-      it.todo('Should have access to properties of the request passed to the constructor')
+      it('Should be instance of Request', () => {
+        const parent = new ParentRequest()
+        const request = createRequest(parent, { mountPoint: '/' })
+
+        expect(request).toBeInstanceOf(Request)
+      })
+
+      it('Should have parent request in the prototype chain', () => {
+        const parent = new ParentRequest()
+        const request = createRequest(parent, { mountPoint: '/' })
+        const firstProto = Object.getPrototypeOf(request)
+        const secondProto = Object.getPrototypeOf(Request.prototype)
+
+        expect(secondProto).toBe(parent)
+      })
     })
 
     describe('#mountPoint', () => {
-      it.todo('Should be read only property')
-      it.todo('Should trail a parent request property "mountPoint"')
-      it.todo('Should not be a string containing double slashes')
-      it.todo('Should has a value passed to the constructor if parent request has no property "mountPoint"')
+
+      it('Should be read only property', () => {
+        const parent = new ParentRequest()
+        const request = createRequest(parent, { mountPoint: '/' })
+
+        const original = request.mountPoint
+        request.mountPoint = {}
+        
+        expect(request.mountPoint).toBe(original)
+      })
+
+      it('Should trail a parent request property "mountPoint"', () => {
+        const parent = new ParentRequest()
+        parent.mountPoint = '/some/path'
+        const mountPoint = '/to'
+        const request = createRequest(parent, { mountPoint })
+
+        expect(request.mountPoint).toBe(parent.mountPoint + mountPoint)
+      })
+
+      it('Should has a value passed to the constructor if parent request has no property "mountPoint"', () => {
+        const parent = new ParentRequest()
+        const mountPoint = '/some/path'
+        const request = createRequest(parent, { mountPoint })
+
+        expect(request.mountPoint).toBe(mountPoint)
+      })
     })
     
     describe('#path', () => {
-      it('Should be a rest of parent url after excluding current mountPoint and query')
-    })
-  })
+    
+      it('Should be read only', () => {
+        const parent = new ParentRequest()
+        const request = createRequest(parent, { mountPoint: '/' })
+      
+        const original = request.path
+        request.path = {}
+      
+        expect(request.path).toBe(original)
+      })
 
-  describe('createRequest', () => {
-    it.todo('Should call a Request constructor with passed values')
+      it('Should be a rest of parent url after excluding current mountPoint query and trailing slash', () => {
+        const parentPath = '/some/path/to'
+        const mountPoint = '/some'
+        const path = parentPath.replace(mountPoint, '')
+        const url = parentPath + '/?param=value'
+        
+        const parent = { url, mountPoint: '/' }
+        const request = createRequest(parent, { mountPoint })
+
+        expect(request.path).toBe(path)
+      })
+    })
   })
 })
