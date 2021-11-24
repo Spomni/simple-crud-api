996175c76fdb6c9339455129bf866e1a9e3418cd
spomni <mikhail.snitko@gmail.com>
2021-11-24 06:07:09 +0300

    test(draft): describe test cases for start-server module

---
+++ test/lib/start-server.spec.js
@@ -0,0 +1,12 @@
+describe('start-server', () => {
+
+  describe('startServer()', () => {
+  
+    it.todo('should call dotenv.config()')
+    it.todo('should create app using createApp function')
+    it.todo('should create server calling http.createServer() with argument returned from th createApp() function')
+    it.todo('should start server listening using default port and host if env variables are not defined')
+    it.todo('should start listening server using constants port and host from process.env if they are defined')
+    it.todo('should print human friendly message to the console when listening is started')
+  })
+})
