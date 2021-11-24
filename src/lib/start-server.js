e15318b5262cef79aa2faf9d9a25b3d220721f65
spomni <mikhail.snitko@gmail.com>
2021-11-24 14:20:39 +0300

    feat: develop start-server module

--- src/lib/start-server.js
+++ src/lib/start-server.js
@@ -1,17 +1,22 @@
 const { createServer } = require('http')
-const { dotenv } = require('./lib/dotenv')
-const { createApp } = require('./app')
+const dotenv = require('./dotenv')
+const { createApp } = require('../app')
 
 dotenv.config()
 
-const port = process.env.PORT || 8080
-const host = process.env.HOST || 'localhost'
-
-const app = createApp()
-const server = createServer(app)
-
 function startServer() {
+
+  const port = process.env.PORT || 3000
+  const host = process.env.HOST || 'localhost'
+  
+  const app = createApp()
+  const server = createServer(app)
+
   server.listen(port, host, () => {
     console.log(`Server is started and is listening on port ${port} of ${host}`)
   })
+}
+
+module.exports = {
+  startServer,
 }
