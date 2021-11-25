aa92c95098e5f58a0550057ce73d950e9423bd39
spomni <mikhail.snitko@gmail.com>
2021-11-25 20:58:38 +0300

    test(draft): describe test cases for person-controller method

---
+++ test/app/controller/person-controller/person-controller.spec.js
@@ -0,0 +1,12 @@
+describe('person-controller', () => {
+
+  describe('personController()', () => {
+
+    it.todo('should throw an UnresolvedRequestError if request path continues');
+    it.todo('should call notImplementedView() if a request has not implemented method');
+    it.todo('should call the getPerson() if a request method is "GET"');
+    it.todo('should call the addPerson() if a request method is "POST"');
+    it.todo('should call the updatePerson() if a request method is "PUT"');
+    it.todo('should call the deletePerson() if a request method is "DELETE"');
+  });
+});
