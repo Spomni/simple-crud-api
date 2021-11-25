3ab7e583ad964ff891d02d383aa6515634f0e5f7
spomni <mikhail.snitko@gmail.com>
2021-11-25 15:39:19 +0300

    test(draft): describe test cases for app module

---
+++ test/app/app.spec.js
@@ -0,0 +1,9 @@
+describe('app', () => {
+  
+  describe('createApp', () => {
+    it.todo('should setup router module to use app error handler by default')
+    it.todo('should return a router instance')
+    it.todo('should create returned instance later than router error handler was setup')
+    it.todo(`should register personControler at the path "/person"`)
+  })
+})
