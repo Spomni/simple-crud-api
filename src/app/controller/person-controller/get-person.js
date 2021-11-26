cf7551a424fd9604f95b7fc074dba1ec12219ac0
spomni <mikhail.snitko@gmail.com>
2021-11-26 08:37:07 +0300

    draft: draft get-person module

--- src/app/controller/person-controller/get-person.js
+++ src/app/controller/person-controller/get-person.js
@@ -1,5 +1,35 @@
-module.exports = {
-  getPerson() {
-    throw new Error('it is not implemented')
+const { validate: isPersonId } = require('../../lib/uuid')
+const crud = require('../../model/crud')
+
+function getPerson(req, res) {
+
+  const [ personId, nextChunk ] = parsePath(req)
+
+  if (nextChunk) {
+    resourceNotFoundView(req, res)
+    return
   }
+
+  if (personId === '') {
+    const personList = crud.person.getAll()
+    personView(req, res, { personList })
+    return
+  }
+
+  if (!isPersonId(personId)) {
+    invalidPersonIdView(req, res, { personId })
+  }
+
+  const person = crud.person.getById(personId)
+
+  if (!person) {
+    resourceNotFoundView(req, res)
+    return
+  }
+
+  personView(req, res, { person })
+}
+
+module.exports = {
+  getPerson,
 }
