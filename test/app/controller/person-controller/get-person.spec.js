a5724921f17c77c23b5b9f814f0eb56f956465e2
spomni <mikhail.snitko@gmail.com>
2021-11-27 12:30:45 +0300

    test(draft): describe test cases for the getPerson() function

---
+++ test/app/controller/person-controller/get-person.spec.js
@@ -0,0 +1,11 @@
+describe('get-person', () => {
+
+  describe('getPerson()', () => {
+
+    it.todo('should call resourceNotFoundView() if path continues after the chunk "/personId"');
+    it.todo('should call personView() with an option "personList" containing an array of the valid person data');
+    it.todo('should call invalidPersonIdView() if the path chunk "/personId" is invalid');
+    it.todo('should call resourceNotFoundView() if a user with passed personId is not found');
+    it.todo('should call personView() with an option "person" containing a valid person data');
+  });
+});
