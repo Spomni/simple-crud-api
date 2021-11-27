2081c5d764f1beb49d6272451577556cbd34633a
spomni <mikhail.snitko@gmail.com>
2021-11-27 13:21:53 +0300

    feat: code the getPerson() funciton

--- src/app/controller/person-controller/get-person.js
+++ src/app/controller/person-controller/get-person.js
@@ -1,33 +1,43 @@
+const { resourceNotFoundView } = require('../../view/404-resource-not-found-view')
+const { personView } = require('../../view/person-view')
+const { invalidPersonIdView } = require('../../view/invalid-person-id-view')
+
 const { validate: isPersonId } = require('../../lib/uuid')
 const crud = require('../../model/crud')
 
+function parsePath(path) {
+  return path.replace('/', '').split('/')
+}
+
 function getPerson(req, res) {
 
-  const [ personId, nextChunk ] = parsePath(req)
+  const [ personId, nextChunk ] = parsePath(req.path)
 
   if (nextChunk) {
     resourceNotFoundView(req, res)
-    return
+    return null
   }
 
   if (personId === '') {
     const personList = crud.person.getAll()
     personView(req, res, { personList })
-    return
+    return personList
   }
 
   if (!isPersonId(personId)) {
     invalidPersonIdView(req, res, { personId })
+    return null
   }
 
   const person = crud.person.getById(personId)
 
   if (!person) {
     resourceNotFoundView(req, res)
-    return
+    return null
   }
 
   personView(req, res, { person })
+  return person
 }
 
 module.exports = {
