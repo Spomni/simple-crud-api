6c65b32030c207cfc58cee96bf350587b8655182
spomni <mikhail.snitko@gmail.com>
2021-11-27 22:48:25 +0300

    draft: draft person-api module

--- src/app/model/crud/person-api.js
+++ src/app/model/crud/person-api.js
@@ -1,22 +1,54 @@
+const { createStorage } = require('../storage')
+const { createPerson } = require('../person')
+const { validate: isPersonId } = require('../../lib/uuid')
+
+const storage = createStorage()
+
+function getAll() {
+  return storage.map((personLike) => createPerson(personLike))
+}
+
+function getById(id) {
+
+  if (!isPersonId(id)) {
+    throw new InvalidPersonIdError(id)
+  }
+
+  return storage.find((personLike) => personLike.id === id)
+}
+
+function create(personLike) {
+  const person = createPerson(personLike)
+  storage.push(person)
+  return createPerson(getById(person.id))
+}
+
+function update(personLike) {
+  const person = getById(personLike.id)
+
+  if (!person) return null
+
+  const stored = storage.find((item) => item.id === person.id)
+  Object.assign(stored, person)
+
+  return createPerson(getById(person.id))
+}
+
+function remove({ id }) {
+  const person = getById(id)
+
+  if (!person) return null
+
+  const index = storage.find((item) => item.id === person.id)
+  storage.splice(index, 1)
+
+  return true
+}
+
 module.exports = {
-
-  getAll() {
-    throw new Error('it is not implemented')
-  },
-
-  getById() {
-    throw new Error('it is not implemented')
-  },
-
-  create() {
-    throw new Error('it is not implemented')
-  },
-
-  update() {
-    throw new Error('it is not implemented')
-  },
-
-  delete() {
-    throw new Error('it is not implemented')
-  },
+ getAll,
+ getById,
+ create,
+ update,
+ delete: remove,
 }
