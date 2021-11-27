11219882651ef6290a80826165d930364166d25f
spomni <mikhail.snitko@gmail.com>
2021-11-27 11:58:47 +0300

    feat: code the model/person module

--- src/app/model/person/person.js
+++ src/app/model/person/person.js
@@ -1,41 +1,60 @@
-const { validate: isPersonId } = require('../../lib/uuid')
+const { StorableInterface } = require('../storable-interface')
+
+const {
+  InvalidIdOptionError,
+  InvalidNameOptionError,
+  InvalidHobbiesOptionError,
+  InvalidAgeOptionError,
+} = require('./person-errors')
+
+const {
+  isPersonId,
+  isPersonName,
+  isPersonAge,
+  isPersonHobbies,
+} = require('./person-helpers')
 
 class Person extends StorableInterface {
-  
+
   constructor({ id, name, age, hobbies }) {
-    this._id = id
-    this._name = name
-    this._age = age
-    this._hobbies = hobbies
+    super()
+
+    this.id = id
+    this.name = name
+    this.age = age
+    this.hobbies = hobbies
   }
-  
+
   get id() { return this._id }
   set id(value) {
     if (!isPersonId(value)) {
-      throw new InvalidPersonIdError(value)
+      throw new InvalidIdOptionError(value)
     }
     this._id = value
   }
-  
+
   get name() { return this._name }
   set name(value) {
     if (!isPersonName(value)) {
-      throw new InvalidPersonNameError(value)
+      throw new InvalidNameOptionError(value)
     }
+    this._name = value
   }
-  
+
   get age() { return this._age }
   set age(value) {
     if (!isPersonAge(value)) {
-      throw new InvalidPersonAgeError(value)
+      throw new InvalidAgeOptionError(value)
     }
+    this._age = value
   }
-  
-  get hobbies() { return this._hobbies }
+
+  get hobbies() { return [...this._hobbies] }
   set hobbies(value) {
     if (!isPersonHobbies(value)) {
-      throw new InvalidPersonHobbiesError(value)
+      throw new InvalidHobbiesOptionError(value)
     }
+    this._hobbies = [...value]
   }
 
   toPlain() {
