92ba357a077dca774c6990fa546d1fcc5ac97180
spomni <mikhail.snitko@gmail.com>
2021-11-27 20:17:12 +0300

    feat: develop person-controller/update-person module

--- src/app/controller/person-controller/update-person.js
+++ src/app/controller/person-controller/update-person.js
@@ -1,5 +1,38 @@
-module.exports = {
-  updatePerson() {
-    throw new Error('it is not implemented')
+const { badRequestView } = require('../../view/400-bad-request-view')
+const { resourceNotFoundView } = require('../../view/404-resource-not-found-view')
+const { personUpdatedView } = require('../../view/person-updated-view')
+
+const crud = require('../../model/crud')
+
+async function updatePerson(req, res) {
+
+  const body = await req.readBody()
+
+  let person = null
+
+  try {
+    personLike = JSON.parse(body)
+
+    person = crud.person.getById(personLike.id)
+
+    if (!person) {
+      resourceNotFoundView(req, res)
+      return null
+    }
+
+    Object.assign(person, personLike)
+
+    person = crud.person.update(person)
+
+  } catch ({ message }) {
+    badRequestView(req, res, { message })
+    return null
   }
+
+  personUpdatedView(req, res, { person })
+  return person
+}
+
+module.exports = {
+  updatePerson,
 }
---
+++ test/app/controller/person-controller/update-person.spec.js
@@ -0,0 +1,137 @@
+const {
+  fromSrc,
+  fromTest,
+} = global.testEnv
+
+jest.mock(fromSrc('app/view/400-bad-request-view'))
+jest.mock(fromSrc('app/view/404-resource-not-found-view'))
+jest.mock(fromSrc('app/view/person-updated-view'))
+jest.mock(fromSrc('app/lib/uuid'))
+jest.mock(fromSrc('app/model/crud/person-api'))
+
+const { badRequestView } = require(fromSrc('app/view/400-bad-request-view'))
+const { resourceNotFoundView } = require(fromSrc('app/view/404-resource-not-found-view'))
+const { personUpdatedView } = require(fromSrc('app/view/person-updated-view'))
+
+const { createRequest } = require(fromSrc('lib/request'))
+const uuid = require(fromSrc('app/lib/uuid'))
+const personApi = require(fromSrc('app/model/crud/person-api'))
+
+const { ParentRequest } = require(fromTest('__helpers/parent-request'))
+const { defaultPersonLike } = require(fromTest('__helpers/default-person-like'))
+
+const { updatePerson } = require(fromSrc('app/controller/person-controller/update-person'))
+
+describe('update-person', () => {
+
+  afterEach(() => jest.resetAllMocks())
+
+  describe('updatePerson', () => {
+
+    it('should call badRequestView() if a request body is not correct json string', async () => {
+
+      const req = createRequest(new ParentRequest(), { mountPoint: '/some' })
+      const res = {}
+
+      const body = '{ id: "uuid" }'
+      let message = null
+
+      try {
+        message = JSON.parse(body)
+      } catch (error) {
+        message = error.message
+      }
+
+      req.end(body)
+
+      const person = await updatePerson(req, res)
+
+      expect(badRequestView).toBeCalledWith(req, res, { message })
+      expect(person).toBe(null)
+    });
+
+    it('should call notFoundView() if person with passed id is not found', async () => {
+
+      const req = createRequest(new ParentRequest(), { mountPoint: '/some'} )
+      const res = {}
+      const body = JSON.stringify(defaultPersonLike)
+      req.end(body)
+
+      personApi.getById.mockReturnValue(null)
+
+      const person = await updatePerson(req, res)
+
+      expect(resourceNotFoundView).toHaveBeenCalledWith(req, res)
+      expect(person).toBe(null)
+    });
+
+    describe('should call badRequestView() if a parsed request body is not valid personLike object', () => {
+
+      it.each(
+        [
+          ['empty object', { empty: true }],
+          ['invalid id', { id: 'not-uuid' }],
+          ['invalid name', { name: ['Joe', 'Pit'] }],
+          ['invalid age', { age: 'nineteen' }],
+          ['invalid hobbies', { hobbies: "singing" }],
+        ]
+      )('%s', async (caseName, extendWith) => {
+
+        const personLike = (!extendWith.empty)
+            ? Object.assign({}, defaultPersonLike, extendWith)
+            : {}
+
+        const message = 'message'
+        const propName = caseName.replace('invalid ', '')
+
+        if (!extendWith.empty) {
+          personApi.getById.mockReturnValue({
+            get [propName]() {},
+            set [propName](value) {
+              throw new Error(message)
+            }
+          })
+        } else {
+          personApi.getById.mockImplementation(() => {
+            throw new Error(message)
+          })
+        }
+
+
+        if (extendWith.id && extendWith.id === 'uuid') {
+          uuid.validate.mockReturnValue(true)
+        } else {
+          uuid.validate.mockReturnValue(false)
+        }
+
+        const req = createRequest(new ParentRequest(), { mountPoint: '/some'} )
+        const body = JSON.stringify(personLike)
+        const res = {}
+        req.end(body)
+
+        const person = await updatePerson(req, res)
+
+        expect(badRequestView).toBeCalledWith(req, res, { message })
+        expect(person).toBe(null)
+      })
+    });
+
+    it('should call personCreatedView with a copy of the request person object', async () => {
+
+      personApi.getById.mockReturnValue(Object.assign({}, defaultPersonLike))
+      personApi.update.mockReturnValue(Object.assign({}, defaultPersonLike))
+
+      const req = createRequest(new ParentRequest(), { mountPoint: '/some'} )
+      const body = JSON.stringify(defaultPersonLike)
+      const res = {}
+      req.end(body)
+
+      const person = await updatePerson(req, res)
+
+      expect(person).not.toBe(null)
+      expect(person).not.toBe(defaultPersonLike)
+      expect(person).toEqual(defaultPersonLike)
+      expect(personUpdatedView).toBeCalledWith(req, res, { person })
+    });
+  });
+});
