7331b773092fc4626deaadd0bc1f2a045e5475f1
spomni <mikhail.snitko@gmail.com>
2021-11-28 11:09:27 +0300

    feat: develop 404-resource-not-found-view module

--- src/app/view/404-resource-not-found-view.js
+++ src/app/view/404-resource-not-found-view.js
@@ -1,5 +1,12 @@
+function resourceNotFoundView(req, res) {
+
+  res.writeHead(404, 'Resource Not Found', {
+    'Content-Type': 'text/plain; charset=UTF-8',
+  })
+
+  res.end()
+}
+
 module.exports = {
-  resourceNotFoundView() {
-    throw new Error('it is not implemented')
-  }
+  resourceNotFoundView,
 }
---
+++ test/app/view/404-resource-not-found-view.spec.js
@@ -0,0 +1,65 @@
+const {
+  fromSrc,
+} = global.testEnv
+
+const matchers = require('jest-extended')
+expect.extend(matchers)
+
+const { resourceNotFoundView } = require(fromSrc('app/view/404-resource-not-found-view'))
+
+const defaultResponse = {
+  writeHead: jest.fn(),
+  write: jest.fn(),
+  end: jest.fn(),
+}
+
+describe('404-resource-not-found-view', () => {
+
+  beforeEach(() => jest.resetAllMocks())
+
+  describe('resourceNotFoundView()', () => {
+
+    it('should set status code 404', () => {
+      const req = {}
+      const res = defaultResponse
+
+      resourceNotFoundView(req, res)
+
+      expect(res.writeHead).toHaveBeenCalled()
+      expect(res.writeHead.mock.calls[0][0]).toBe(404)
+    });
+
+    it('should set status message "Resource Not Found"', () => {
+      const req = {}
+      const res = defaultResponse
+
+      resourceNotFoundView(req, res)
+
+      expect(res.writeHead).toHaveBeenCalled()
+      expect(res.writeHead.mock.calls[0][1]).toBe('Resource Not Found')
+    });
+
+    it('should set content-type "text/plain; charset=UTF-8"', () => {
+      const req = {}
+      const res = defaultResponse
+
+      resourceNotFoundView(req, res)
+
+      expect(res.writeHead).toHaveBeenCalled()
+      expect(res.writeHead.mock.calls[0][2]).toBeObject()
+
+      const contentType = res.writeHead.mock.calls[0][2]['Content-Type']
+      expect(contentType).toBe('text/plain; charset=UTF-8')
+    });
+
+    it('should be ended without body', () => {
+      const req = {}
+      const res = defaultResponse
+
+      resourceNotFoundView(req, res)
+
+      expect(res.write).not.toHaveBeenCalled()
+      expect(res.end).toHaveBeenCalledWith()
+    });
+  });
+});
