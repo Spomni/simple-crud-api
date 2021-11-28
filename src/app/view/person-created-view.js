1a3681c37bc4301a8c0d00dc73ac417f48a23deb
spomni <mikhail.snitko@gmail.com>
2021-11-28 12:37:44 +0300

    feat: develop person-created-view module

--- src/app/view/person-created-view.js
+++ src/app/view/person-created-view.js
@@ -1,5 +1,9 @@
+const { personView } = require('./person-view')
+
+function personCreatedView(req, res, { person }) {
+  personView(req, res, { person, statusCode: 201 })
+}
+
 module.exports = {
-  personCreatedView() {
-    throw new Error('it is not implemented')
-  }
+  personCreatedView,
 }
